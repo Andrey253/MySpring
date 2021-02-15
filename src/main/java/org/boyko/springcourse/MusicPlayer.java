544a58962db20d9f467923faf49c4a7a143a629c
Andrey253 <andrey253@yandex.ru>
2021-02-16 00:23:47 +0700

    2

--- src/main/java/org/boyko/springcourse/MusicPlayer.java
+++ src/main/java/org/boyko/springcourse/MusicPlayer.java
@@ -2,10 +2,35 @@ package org.boyko.springcourse;
 
 public class MusicPlayer {
     private Music music;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getVolume() {
+        return volume;
+    }
+
+    public void setVolume(Integer volume) {
+        this.volume = volume;
+    }
+
+    private Integer volume;
 
     public MusicPlayer(Music music){
         this.music = music;
     }
+    public MusicPlayer(){    }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
     public void playMusic(){
         System.out.println(music.getSong());
     }
